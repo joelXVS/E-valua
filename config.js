1b97ebfbb32330cd1f51dae2d39502ce30bcea45
joelXVS <joseperea454214@gmail.com>
2025-12-22 23:39:52 -0500

    actualizar URL de APPSCRIPT II

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - Configuración global
 const SYSTEM_CONFIG = {
   // URL de tu App Script desplegado como Web App
-  DRIVE_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbznSv-TFuRQ1tmstPrhFNLnpk2tx8o-gTTF34MJWlV8NkwA6XebzU1Ebh49gfPtORG1/exec',
+  DRIVE_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyJLPnnYlBOx8Y-dG6eTlqZJmR6tOdHc2_Fj7f-mAn7L13QFiJqzFkb5kKABjMjiwdv/exec',
   
   // Archivos de base de datos en Google Drive
   DATABASE_FILES: {
